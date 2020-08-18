6b99719dbfa0ffa01e70b0d2805c56184ba22941
Brandon J McKay <bmckay@tractionguest.com>
2020-08-17 20:40:10 -0700

    Add scopes and filters for Product

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,2 +1,30 @@
 class V1::ProductsController < V1Controller
+
+  def index
+    render json: paginate(queried_products),
+           each_serializer: V1::ProductSerializer,
+           root: 'products',
+           adapter: :json,
+           include: filter_params[:include],
+           meta: pagination_meta,
+           meta_key: META_KEY
+  end
+
+  private
+
+  def filter_params
+    params.permit(:department_id, :query, :promo_code, :include)
+  end
+
+  def queried_products
+    @queried_products ||= begin
+      # Had a consistent bug with this, so had to do without :/
+      # initialize_filterrific(
+      #     Product,
+      #     params.permit(:include),
+      #     persistence_id: false,
+      #     sanitize_params: true
+      #   ).find.includes(:department, :promotions)
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,17 @@ class Product < ApplicationRecord
   belongs_to :department
   has_many :products_promotions
   has_many :promotions, through: :products_promotions
+
+  scope :department_id, ->(department_id) { where(department_id: department_id.to_i) }
+  scope :query, ->(name) { where('products.name ilike ?', "%#{name}%") }
+  scope :promo_code, ->(code) { joins(:promotions).where('promotions.code = ?', code) }
+
+  filterrific(
+    default_filter_params: {},
+    available_filters: [
+      :department_id,
+      :query,
+      :promo_code,
+    ]
+  )
 end
